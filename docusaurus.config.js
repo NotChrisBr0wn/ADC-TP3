a72b50dec391999caf97cf1d820fc5c898b34ed7
Rafael Santos <a84788@ualg.pt>
2024-01-05 01:07:13 +0000

    Foi adicionado o link do Github.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/NotChrisBr0wn/adc-tp3',
             label: 'GitHub',
             position: 'right',
           },
