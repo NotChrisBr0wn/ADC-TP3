03fe1391af2de5e8febf0fc4705b6b3f6dfe1ef1
Daniel BATISTA <batistadaniel3007@gmail.com>
2025-05-26 16:19:19 +0100

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Food Timer',
-  tagline: 'Cozinhe facilmente',
+  tagline: 'Cozinhe com facilidade',
   favicon: 'img/cooktimer.ico',
 
   // Set the production url of your site here
