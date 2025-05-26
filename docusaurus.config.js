6f0cc6b31d570082fd06fe256f46d71c778786cf
Daniel BATISTA <batistadaniel3007@gmail.com>
2025-05-26 16:01:06 +0100

    Alterações na pagina inicial

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Food Timer App',
+  title: 'Food Timer',
   tagline: 'Cozinhe facilmente',
   favicon: 'img/cooktimer.ico',
 
